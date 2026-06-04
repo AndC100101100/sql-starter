6a2d484e49b1bf1ff013fcd524ddd11119c309d1
Andres <andrescastrogonzal245@outlook.com>
2026-06-04 01:53:09 -0500

    operators, using UNION to emulate a FULL JOIN

--- mysql-operators/UNION.sql
+++ mysql-operators/UNION.sql
@@ -2,3 +2,14 @@
 SELECT DISTINCT country AS "countries & currencies" FROM customers
 UNION
 SELECT DISTINCT currency AS "countries & currencies" FROM orders;
+
+-- using UNION as a FULL JOIN
+
+SELECT customers.customerId, customers.firstName, customers.lastName, orders.orderId, orders.currency, orders.total
+FROM customers
+LEFT JOIN orders ON customers.customerId = orders.customerId
+UNION
+SELECT customers.customerId, customers.firstName, customers.lastName, orders.orderId, orders.currency, orders.total
+FROM customers
+RIGHT JOIN orders ON customers.customerId = orders.customerId
+ORDER BY customerId;
