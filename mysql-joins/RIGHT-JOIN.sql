33ccaf189d70ff47b344390142160581472f4407
Andres <andrescastrogonzal245@outlook.com>
2026-06-04 01:33:22 -0500

    right joins

--- mysql-joins/RIGHT-JOIN.sql
+++ mysql-joins/RIGHT-JOIN.sql
@@ -1,4 +1,11 @@
+-- same results as the left join query
 SELECT customers.customerId, customers.firstName, customers.lastName, orders.orderId, orders.currency, orders.total
 FROM orders
 RIGHT JOIN customers ON customers.customerId = orders.customerId
+ORDER BY customers.customerId ASC;
+
+-- what right join would be 
+SELECT customers.customerId, customers.firstName, customers.lastName, orders.orderId, orders.currency, orders.total
+FROM customers
+RIGHT JOIN orders ON customers.customerId = orders.customerId
 ORDER BY customers.customerId ASC;
